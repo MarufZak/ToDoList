d7986b7ac58cb1eb5356f4eea7cce513b8b3d4b6
Maruf <maruf@maruf.maruf>
2021-01-03 09:49:09 +0500

    allDone

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
-$('.btn-primary').click(function (e) {
-    e.preventDefault()  
+$('.btn-primary').click(function () {
     let text = $('.form-control').val();
-    let copy = $('.list-group-item').last().clone()
-    let btn = $('.remove').first().clone();
-        if (text == '') {
-            $('.list').children().last().before(copy).html('Пусто').append(btn)
-            $('.form-control').val('');
-        } else {
-            $('.list').children().last().before(copy).html(text).append(btn)
-            $('.form-control').val('');
-        }
-        let list = $('.list').children()
-        if (list.length == 0) {
-            $('.list').wrapInner(`<li class="list-group-item">${text}<button class = "btn remove">X</button></li>`)
-            console.log(1);
-        }
-    
+    $('.list').children().last().after(`<li class="list-group-item">${text}<button class = "btn remove">X</button></li>`);
+    $('.form-control').val('')
+    let list = $('.list').children()
+    if (list.length == 0) {
+        $('.list').wrapInner(`<li class="list-group-item">${text}<button class = "btn remove">X</button></li>`)
+    }
     $('.remove').click(function () {  
         $($(this).parent()).remove()
     })
 })
 $('.remove').click(function () {  
     $($(this).parent()).remove()
-})
+})  
+
